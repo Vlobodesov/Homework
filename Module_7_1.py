c74b1329e5854812bac861b867d0bd778d5f81e1
Vlobodesov <lobodesovviktor@gmail.com>
2025-01-27 18:35:43 +0500

    Update Module_7_1.py

--- Module_7_1.py
+++ Module_7_1.py
@@ -12,18 +12,27 @@ class Product:
 
 class Shop(Product):
     __file_name = 'product.txt'
-    # def __init__(self):
-    #     super().__init__(self)
+
     def get_products(self):
         file = open(self.__file_name, 'r')
         pprint(file.read())
         file.close()
         return f'{self.__file_name}'
 
+    def add(self, *products):
+        for prod in products:
+            if prod in products:
+                return f'Продукт {self.name} уже есть в магазине'
+            else:
+                file = open(self.__file_name, 'a')
+                pprint(file.write(prod))
+                file.close()
 
 
 
-s1 = Shop()
+
+
+s1 = Shop('Potato', 50.5, 'Vegetables')
 
 p1 = Product('Potato', 50.5, 'Vegetables')
 
@@ -32,16 +41,16 @@ p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
 
-print(s1)
 print(p2) # __str__
 
 
 
-# s1.add(p1, p2, p3)
-#
-#
-#
-# print(s1.get_products())
+s1.add(p1, p2, p3)
+
+
+
+print(s1.get_products())
+
 
 
 
