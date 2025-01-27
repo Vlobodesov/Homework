7dd4dc7a12567886d8a36c5a25080111bc590579
Vlobodesov <lobodesovviktor@gmail.com>
2025-01-27 17:58:07 +0500

    Update Module_7_1.py

--- Module_7_1.py
+++ Module_7_1.py
@@ -12,15 +12,36 @@ class Product:
 
 class Shop(Product):
     __file_name = 'product.txt'
-    def __init__(self):
+    # def __init__(self):
+    #     super().__init__(self)
     def get_products(self):
-        open(self.__file_name, 'r')
-        pprint(self.__file_name.read())
-        self.__file_name.close()
+        file = open(self.__file_name, 'r')
+        pprint(file.read())
+        file.close()
         return f'{self.__file_name}'
 
-    def add(self,*products):
-        if self.products not in self.__file_name:
+
+
+
+s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+
+print(s1)
+print(p2) # __str__
+
+
+
+# s1.add(p1, p2, p3)
+#
+#
+#
+# print(s1.get_products())
 
 
 
